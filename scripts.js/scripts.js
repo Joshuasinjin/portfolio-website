c7ae03844189693e47c3b36cb16d255a2819806b
Joshuasinjin <84568769+Joshuasinjin@users.noreply.github.com>
2022-02-22 12:55:30 -0800

    gen

--- scripts.js/scripts.js
+++ scripts.js/scripts.js
@@ -1,10 +1,1 @@
 console.log("Hello! Wecome to my portfolio website.")
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
-}
